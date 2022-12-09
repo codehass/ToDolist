b6c6b55b03df858f13254b0c124ff68ce4fd09fe
Hassan EL OUARDY <92850366+codehass@users.noreply.github.com>
2022-12-09 21:57:33 +0100

    Update interaction.js

--- src/Modules/interaction.js
+++ src/Modules/interaction.js
@@ -1,14 +1,15 @@
 import MainFunc from './method.js';
 
 export default class Interact {
-  static changeCompletedToDo = (checkstat, id) => {
-    const taskList = MainFunc.getListFromStorage();
-    taskList[id].completed = checkstat;
-    MainFunc.addToStore(taskList);
-    MainFunc.genList();
-  };
+    static changeCompletedToDo = (checkstat, id) => {
+      const taskList = MainFunc.getListFromStorage();
+      taskList[id].completed = checkstat;
+      MainFunc.addToStore(taskList);
+      MainFunc.genList();
+    }
 
-  static checkStatusEvent = () => document.querySelectorAll('.check').forEach((checkbox) => checkbox.addEventListener('change', () => {
+    static checkStatusEvent = () => (
+      document.querySelectorAll('.check').forEach((checkbox) => checkbox.addEventListener('change', () => {
         let checkstat;
         let id;
         if (checkbox.id > 0) {
@@ -24,14 +25,15 @@ export default class Interact {
         }
 
         this.changeCompletedToDo(checkstat, id);
-      }));
+      }))
+    )
 
-  static clearCompleted = () => {
-    let taskList = MainFunc.getListFromStorage();
+    static clearCompleted = () => {
+      let taskList = MainFunc.getListFromStorage();
 
-    taskList = taskList.filter((item) => item.completed !== true);
-    MainFunc.indexNew(taskList);
-    MainFunc.addToStore(taskList);
-    MainFunc.genList();
-  };
+      taskList = taskList.filter((item) => item.completed !== true);
+      MainFunc.indexNew(taskList);
+      MainFunc.addToStore(taskList);
+      MainFunc.genList();
+    }
 }
