261c46501441912c5e9609cebe199fbb63dc616f
Hassan EL OUARDY <92850366+codehass@users.noreply.github.com>
2022-12-09 21:58:53 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 
 import MainFunc from './modules/method.js';
+import Interact from './modules/interaction.js';
 
 const userInput = document.querySelector('.add-item');
 const addActivity = document.querySelector('.add-activity');
@@ -9,6 +10,15 @@ userInput.addEventListener('submit', (e) => {
   e.preventDefault();
   MainFunc.addTodo(addActivity.value);
   addActivity.value = '';
+});
+
+document.querySelector('.clearBtn').addEventListener('click', Interact.clearCompleted);
+
+window.addEventListener('load', () => {
+  document.addEventListener('listUpdated', () => {
+    Interact.checkStatusEvent();
+  }, false);
+  Interact.checkStatusEvent();
 });
 
 MainFunc.genList();
