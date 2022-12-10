3739c4b39c0ea891f6f5067107fb501bf9c92733
iamhassan <hassan.elouardy06@gmail.com>
2022-12-10 12:59:14 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,112 @@
 import './style.css';
+const submit = document.querySelector('.sub');
+const tasksDiv = document.querySelector('.tasks');
+let input = document.querySelector('.input');
 
-import MainFunc from './method.js';
+let tasks = [];
 
-const userInput = document.querySelector('.add-item');
-const addActivity = document.querySelector('.add-activity');
+if (localStorage.getItem('tasks')) {
+  tasks = JSON.parse(localStorage.getItem('tasks'));
+}
 
-userInput.addEventListener('submit', (e) => {
-  e.preventDefault();
-  MainFunc.addTodo(addActivity.value);
-  addActivity.value = '';
-});
+getDataStorage();
 
-MainFunc.genList();
+//Add tasks
+submit.onclick = function () {
+  if (input.value !== '') {
+    addTask(input.value);
+    input.value = '';
+  }
+};
+
+function addTask(taskText) {
+  // Task Data
+  const task = {
+    id: Date.now(),
+    description: taskText,
+    completed: false,
+    index: 0,
+  };
+
+  tasks.push(task);
+  // Add Tasks To Page
+  addToPage(tasks);
+  // Add Tasks To Local Storage
+  addToStorage(tasks);
+  // console.log(tasks);
+  // for (let i = 0; i < tasks.length; i++) {
+  //   tasks[i].index = i + 1;
+  // }
+}
+
+function showMenu() {
+  console.log();
+}
+
+//fonction to add tasks elements to the page
+function addToPage(tasks) {
+  tasksDiv.innerHTML = '';
+  tasks.forEach((i) => {
+    let li = document.createElement('li');
+    li.className = 'tskList';
+    if (i.completed == true) {
+      li.className = 'tskList done';
+    }
+    let inp = document.createElement('input');
+    inp.setAttribute('type', 'checkbox');
+    li.appendChild(inp);
+    let div = document.createElement('div');
+    div.className = 'task';
+    li.appendChild(div);
+    let p = document.createElement('p');
+    p.innerHTML = `${i.description}`;
+    div.appendChild(p);
+    let iEdit = document.createElement('i');
+    iEdit.className = 'edit fa-solid fa-ellipsis-vertical';
+
+    iEdit.setAttribute('onclick', showMenu(this));
+    div.appendChild(iEdit);
+
+    let idelet = document.createElement('i');
+    idelet.className = 'delet fa-solid fa-trash';
+    div.appendChild(idelet);
+    tasksDiv.appendChild(li);
+
+    iEdit.addEventListener('click', (e) => {
+      e.target.classList.add('show');
+    });
+  });
+}
+
+let delettask = document.querySelector('.delet.show');
+
+console.log(delettask);
+
+// function to add tasks to local storrage
+function addToStorage(tasks) {
+  window.localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function getDataStorage() {
+  let data = window.localStorage.getItem('tasks');
+  if (data) {
+    let Tasks = JSON.parse(data);
+    addToPage(Tasks);
+  }
+}
+
+// const sortTasks = tasks.sort((a, b) => a.index - b.index);
+
+// function listTasks() {
+//   sortTasks.forEach((i) => {
+//     Tasks.innerHTML += `<li>
+//     <input type="checkbox" />
+//     <div class="task">
+//       <p>${i.description}</p>
+//       <i class="fa-solid fa-ellipsis-vertical"></i>
+//     </div>
+//   </li>`;
+//   });
+// }
+
+// listTasks();
